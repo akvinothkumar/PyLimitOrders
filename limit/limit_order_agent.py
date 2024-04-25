de01412759e2a89bd4b595a09ca50ea146038799
VINOTHKUMAR AK <akvinothkumar881996@gmail.com>
2024-04-25 15:04:52 +0530

    code_fix_commit_limit_order

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -1,6 +1,12 @@
+import sys
+import os
+# sys.path.append('C:\\Users\\SPLPT1\\test\\PyLimitOrders\\trading_framework')
+# sys.path.append('C:\Users\SPLPT1\test\PyLimitOrders\trading_framework')
+# sys.path.append('\\trading_framework')
+# print(sys.path)
+# print(os.getcwd())
 from trading_framework.execution_client import ExecutionClient
 from trading_framework.price_listener import PriceListener
-
 
 class LimitOrderAgent(PriceListener):
 
@@ -13,4 +19,21 @@ class LimitOrderAgent(PriceListener):
 
     def on_price_tick(self, product_id: str, price: float):
         # see PriceListener protocol and readme file
-        pass
+        self.add_order(product_id=product_id, current_price= price)
+        # pass
+
+    def add_order(self, product_id, order_mode, limit_amount, amount, current_price):
+        self.current_price = current_price
+        self.product_id = product_id
+        self.order_mode = order_mode        
+        self.limit_amount = limit_amount
+        if self.order_mode == "buy":
+            self.amount = amount
+            ExecutionClient.buy(self.product_id, self.amount, self.order_mode, self.current_price)
+        if self.order_mode == "sell":
+            self.amount = amount
+            ExecutionClient.sell(self.product_id, self.amount, self.order_mode, self.current_price)
+
+# if __name__ == "__main__":
+limit_ord_agent = LimitOrderAgent()
+limit_ord_agent.on_price_tick("101",'1000')
--- trading_framework/execution_client.py
+++ trading_framework/execution_client.py
@@ -2,25 +2,42 @@ from typing import Protocol
 
 
 class ExecutionException(Exception):
-    pass
+    msg = "Check the data properly to ignore the errors"
 
 
 class ExecutionClient(Protocol):
 
-    def buy(self, product_id: str, amount: int):
+    def buy(self, product_id: str, amount: int, order_mode: str, current_price: int):
         """
         Execute a buy order, throws ExecutionException on failure
         :param product_id: the product to buy
         :param amount: the amount to buy
         :return: None
         """
-        ...
+        try:
+            self.execute_order(order_mode, product_id, amount, current_price)
+        except ExecutionException as e:
+            print("Exception:", e.msg)
+        
 
-    def sell(self, product_id: str, amount: int):
+    def sell(self, product_id: str, amount: int, order_mode: str, current_price: int):
         """
         Execute a sell order, throws ExecutionException on failure
         :param product_id: the product to sell
         :param amount: the amount to sell
         :return: None
+        amount: existing_order_price
+        order_mode: flag_value_to_decide_buy_or_sell
         """
-        ...
+        try:
+            self.execute_order(order_mode, product_id, amount, current_price)
+        except ExecutionException as e:
+            print("Exception:", e.msg)   
+
+    def execute_order(self, order_mode, product_id, amount, current_price):
+        if order_mode == "buy":
+            if current_price <= amount:
+                print(f"Order bought successfully! Details: Product ID:{product_id}, Amount:{amount}")
+        else:
+            if current_price > amount:
+                print(f"Order sold successfully! Details: Product ID:{product_id}, Amount:{amount}")
